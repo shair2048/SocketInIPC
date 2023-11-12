c6c363ebccc6aff817783c8290495e6f2e88a95f
SHair <sonhai.it.2048@gmail.com>
2023-11-12 19:51:13 +0700

    Sua lai cho phep server/client chat vs nhau den khi dat dieu kien dung

--- client.c
+++ client.c
@@ -4,44 +4,61 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <sys/un.h>
 
-#define PORT 8080
+#define SOCKET_PATH "/tmp/demo_socket"
 
 int main() {
     int client_socket;
-    struct sockaddr_in server_address;
+    struct sockaddr_un server_address;
 
     // Tạo socket
-    if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    if ((client_socket = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(PORT);
-
-    // Chuyển đổi địa chỉ từ địa chỉ IP sang địa chỉ thực sự
-    if (inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr) <= 0) {
-        perror("Invalid address/ Address not supported");
-        exit(EXIT_FAILURE);
-    }
+    // Thiết lập địa chỉ server
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sun_family = AF_UNIX;
+    strncpy(server_address.sun_path, SOCKET_PATH, sizeof(server_address.sun_path) - 1);
 
     // Kết nối đến server
-    if (connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
+    if (connect(client_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == -1) {
         perror("Connection failed");
         exit(EXIT_FAILURE);
     }
 
-    char *message = "Hello from client";
-    send(client_socket, message, strlen(message), 0);
-    printf("Message sent to server: %s\n", message);
+    char message[1024];
+    int continue_sending = 1;  // Biến kiểm soát quá trình truyền thông
 
-    char buffer[1024] = {0};
-    read(client_socket, buffer, sizeof(buffer));
-    printf("Received message from server: %s\n", buffer);
+    // Vòng lặp để nhập và gửi tin nhắn
+    while (continue_sending) {
+        // Nhập tin nhắn từ người dùng
+        printf("Enter your message (or type 'quit' to exit): ");
+        fgets(message, sizeof(message), stdin);
 
+        // Gửi tin nhắn đến server
+        send(client_socket, message, strlen(message), 0);
+
+        // Kiểm tra điều kiện dừng (nếu tin nhắn là "quit")
+        if (strcmp(message, "quit\n") == 0) {
+            printf("Client is quitting.\n");
+            continue_sending = 0;  // Dừng quá trình truyền thông
+        } else {
+            // Nhận phản hồi từ server
+            ssize_t received = recv(client_socket, message, sizeof(message) - 1, 0);
+            if (received <= 0) {
+                perror("Receive failed");
+                break;
+            }
+
+            message[received] = '\0';
+            printf("Received response from server: %s\n", message);
+        }
+    }
+
+    // Đóng socket
     close(client_socket);
 
     return 0;
--- server.c
+++ server.c
@@ -4,52 +4,86 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <sys/un.h>
 
-#define PORT 8080
+#define SOCKET_PATH "/tmp/demo_socket"
 
 int main() {
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
+    int server_socket, client_socket;
+    struct sockaddr_un server_address, client_address;
+    socklen_t client_address_len = sizeof(client_address);
 
     // Tạo socket
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    if ((server_socket = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    // Thiết lập địa chỉ server
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sun_family = AF_UNIX;
+    strncpy(server_address.sun_path, SOCKET_PATH, sizeof(server_address.sun_path) - 1);
 
-    // Gán địa chỉ và cổng cho socket
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    // Bind socket đến địa chỉ
+    if (bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == -1) {
         perror("Bind failed");
         exit(EXIT_FAILURE);
     }
 
     // Lắng nghe kết nối
-    if (listen(server_fd, 3) < 0) {
+    if (listen(server_socket, 5) == -1) {
         perror("Listen failed");
         exit(EXIT_FAILURE);
     }
 
+    printf("Server is listening...\n");
+
     // Chấp nhận kết nối mới
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+    if ((client_socket = accept(server_socket, (struct sockaddr*)&client_address, &client_address_len)) == -1) {
         perror("Accept failed");
         exit(EXIT_FAILURE);
     }
 
-    char buffer[1024] = {0};
-    read(new_socket, buffer, sizeof(buffer));
-    printf("Received message from client: %s\n", buffer);
+    printf("Client connected\n");
 
-    char *message = "Hello from server";
-    send(new_socket, message, strlen(message), 0);
+    char message[1024];
+    int continue_sending = 1;  // Biến kiểm soát quá trình truyền thông
 
-    close(new_socket);
-    close(server_fd);
+    // Vòng lặp để nhận và gửi tin nhắn
+    while (continue_sending) {
+        // Nhận tin nhắn từ client
+        ssize_t received = recv(client_socket, message, sizeof(message) - 1, 0);
+        if (received <= 0) {
+            perror("Receive failed");
+            break;
+        }
+
+        message[received] = '\0';
+        printf("Received message from client: %s\n", message);
+
+        // Kiểm tra điều kiện dừng (ví dụ: nếu tin nhắn là "quit")
+        if (strcmp(message, "quit") == 0) {
+            printf("Server is quitting.\n");
+            continue_sending = 0;  // Dừng quá trình truyền thông
+        } else {
+            // Gửi phản hồi về client
+            send(client_socket, message, strlen(message), 0);
+
+            // Nhập tin nhắn từ server
+            printf("Enter your response (or type 'quit' to exit): ");
+            fgets(message, sizeof(message), stdin);
+
+            // Gửi tin nhắn đến client
+            send(client_socket, message, strlen(message), 0);
+        }
+    }
+
+    // Đóng socket
+    close(client_socket);
+    close(server_socket);
+
+    // Xóa socket file
+    unlink(SOCKET_PATH);
 
     return 0;
 }
