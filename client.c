fb1da8633f5de5489f3ead28f6b3db4b45e237e6
lvnh2003 <lanhnn.21it@vku.udn.vn>
2023-11-19 09:28:52 +0700

    chat text have space

--- client.c
+++ client.c
@@ -20,9 +20,7 @@ void *receive_messages(void *arg) {
             break;
         }
 
-        // message[received] = '\0';
-        // printf("\x1b[1A"); // Di chuyển lên 1 dòng
-        // printf("\x1b[K");  // Xóa dòng hiện tại
+        message[received] = '\0';
         printf("Received response from server: %s\n", message);
     }
 
@@ -34,30 +32,25 @@ int main() {
     int client_socket;
     struct sockaddr_un server_address;
 
-    // Xác minh rằng file socket của server đã tồn tại
     if (access(SOCKET_PATH, F_OK) == -1) {
         perror("Server socket does not exist");
         exit(EXIT_FAILURE);
     }
 
-    // Tạo socket client
     if ((client_socket = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    // Thiết lập địa chỉ server
     memset(&server_address, 0, sizeof(server_address));
     server_address.sun_family = AF_UNIX;
     strncpy(server_address.sun_path, SOCKET_PATH, sizeof(server_address.sun_path) - 1);
 
-    // Kết nối đến server
     if (connect(client_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == -1) {
         perror("Connection failed");
         exit(EXIT_FAILURE);
     }
 
-    // Tạo luồng riêng để nhận phản hồi từ server
     pthread_t thread;
     pthread_create(&thread, NULL, receive_messages, &client_socket);
     pthread_detach(thread);
@@ -65,21 +58,25 @@ int main() {
     char message[1024];
 
     while (1) {
-        // Nhập tin nhắn từ người dùng
-        // printf("Enter your message (or type 'quit' to exit): ");
+        // Read input using fgets to handle spaces
+        printf("Enter your message (or type 'quit' to exit): ");
         fgets(message, sizeof(message), stdin);
 
-        // Gửi tin nhắn đến server
+        // Trim trailing newline character
+        size_t len = strlen(message);
+        if (len > 0 && message[len - 1] == '\n') {
+            message[len - 1] = '\0';
+        }
+
+        // Send the message to the server
         send(client_socket, message, strlen(message), 0);
 
-        // Kiểm tra điều kiện dừng (nếu tin nhắn là "quit")
-        if (strcmp(message, "quit\n") == 0) {
+        if (strcmp(message, "quit") == 0) {
             printf("Client is quitting.\n");
-            break;  // Dừng quá trình truyền thông
+            break;
         }
     }
 
-    // Đóng socket client
     close(client_socket);
 
     return 0;
--- server.c
+++ server.c
@@ -8,16 +8,18 @@
 #include <pthread.h>
 
 #define SOCKET_PATH "/tmp/demo_socket"
+#define MAX_CLIENTS 10
 
-int client_sockets[10]; // Mảng chứa socket của các client
-int num_clients = 0; // Số lượng client hiện tại
+int client_sockets[MAX_CLIENTS];
+int num_clients = 0;
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void *handle_client(void *arg) {
     int client_socket = *(int *)arg;
     char message[1024];
 
     while (1) {
-        // Nhận tin nhắn từ client
         ssize_t received = recv(client_socket, message, sizeof(message) - 1, 0);
         if (received <= 0) {
             perror("Receive failed");
@@ -27,23 +29,23 @@ void *handle_client(void *arg) {
         message[received] = '\0';
         printf("Received message from client: %s\n", message);
 
-        // Kiểm tra điều kiện dừng (ví dụ: nếu tin nhắn là "quit")
         if (strcmp(message, "quit") == 0) {
             printf("Client is quitting.\n");
-            break;  // Kết thúc kết nối của client
+            break;
         } else {
-            // Gửi tin nhắn đến tất cả client khác
+            pthread_mutex_lock(&mutex);
             for (int i = 0; i < num_clients; i++) {
                 if (client_sockets[i] != client_socket) {
                     send(client_sockets[i], message, strlen(message), 0);
                 }
             }
+            pthread_mutex_unlock(&mutex);
         }
     }
 
     close(client_socket);
 
-    // Loại bỏ client_socket khỏi mảng
+    pthread_mutex_lock(&mutex);
     for (int i = 0; i < num_clients; i++) {
         if (client_sockets[i] == client_socket) {
             for (int j = i; j < num_clients - 1; j++) {
@@ -53,46 +55,40 @@ void *handle_client(void *arg) {
             break;
         }
     }
+    pthread_mutex_unlock(&mutex);
 
     return NULL;
 }
 
 int main() {
     int server_socket;
-    struct sockaddr_un server_address;
+    struct sockaddr_un server_address, client_address;
     socklen_t client_address_len;
-    struct sockaddr_un client_address;
 
-    // Kiểm tra xem file socket đã tồn tại chưa, nếu có thì xóa
     if (access(SOCKET_PATH, F_OK) != -1) {
         unlink(SOCKET_PATH);
     }
 
-    // Tạo socket
     if ((server_socket = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    // Sử dụng tùy chọn SO_REUSEADDR
     int reuse = 1;
     if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(int)) == -1) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
 
-    // Thiết lập địa chỉ server
     memset(&server_address, 0, sizeof(server_address));
     server_address.sun_family = AF_UNIX;
     strncpy(server_address.sun_path, SOCKET_PATH, sizeof(server_address.sun_path) - 1);
 
-    // Bind socket đến địa chỉ
     if (bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == -1) {
         perror("Bind failed");
         exit(EXIT_FAILURE);
     }
 
-    // Lắng nghe kết nối
     if (listen(server_socket, 5) == -1) {
         perror("Listen failed");
         exit(EXIT_FAILURE);
@@ -102,29 +98,29 @@ int main() {
 
     while (1) {
         client_address_len = sizeof(client_address);
-        // Chấp nhận kết nối mới
         int client_socket = accept(server_socket, (struct sockaddr*)&client_address, &client_address_len);
         if (client_socket == -1) {
             perror("Accept failed");
             continue;
         }
 
-        // Thêm client_socket vào mảng
-        client_sockets[num_clients] = client_socket;
-        num_clients++;
+        pthread_mutex_lock(&mutex);
+        if (num_clients < MAX_CLIENTS) {
+            client_sockets[num_clients] = client_socket;
+            num_clients++;
+            printf("New client connected!!!\n");
 
-        printf("New client connected!!!\n");
-
-        // Tạo một luồng mới để xử lý client
-        pthread_t thread;
-        pthread_create(&thread, NULL, handle_client, &client_socket);
-        pthread_detach(thread);
+            pthread_t thread;
+            pthread_create(&thread, NULL, handle_client, &client_socket);
+            pthread_detach(thread);
+        } else {
+            printf("Maximum number of clients reached. Connection rejected.\n");
+            close(client_socket);
+        }
+        pthread_mutex_unlock(&mutex);
     }
 
-    // Đóng socket
     close(server_socket);
-
-    // Xóa socket file
     unlink(SOCKET_PATH);
 
     return 0;
